040d5794e87a5a0343cc8fdac5f03b220ab79983
Andrew Plaza <github@andrewplaza.dev>
2026-01-24 16:11:20 -0500

    split monitoring into kube-stack.nix

---
+++ deployments/kubenix/monitoring/kube-stack.nix
@@ -0,0 +1,215 @@
+{ kubenix, flake, ... }:
+let
+  ns = "monitoring";
+in
+{
+  imports = with kubenix.modules;
+    [ k8s helm submodules ];
+  submodules.imports = [ ../lib/namespaced.nix ];
+  submodules.instances.monitoring = {
+    submodule = "namespaced";
+    args.kubernetes = {
+      helm.releases = {
+        kube-prometheus-stack = {
+          chart = kubenix.lib.helm.fetch {
+            repo = "https://prometheus-community.github.io/helm-charts";
+            chart = "kube-prometheus-stack";
+            version = "80.9.1";
+            sha256 = "sha256-edZNrpeFAPOesVC+BFBbAAafFyZc/m5Dy26lJpzakG0=";
+          };
+          namespace = ns;
+          # includeCRDs = true; fails
+          values = {
+
+            kubeControllerManager.enabled = false;
+            kubeScheduler.enabled = false;
+            kubeProxy.enabled = false;
+            kubeEtcd.enabled = false;
+            coreDns.enabled = false; # ns conflict
+
+            prometheus = {
+              prometheusSpec = {
+                # externalUrl = "https://prometheus.${flake.lib.hostname}";
+                serviceMonitorSelectorNilUsesHelmValues = false;
+                podMonitorSelectorNilUsesHelmValues = false;
+                storageSpec.volumeClaimTemplate.spec = {
+                  storageClassName = "longhorn-static";
+                  accessModes = [ "ReadWriteOnce" ];
+                  resources.requests.storage = "35Gi";
+                };
+                additionalScrapeConfigs = [
+                  {
+                    job_name = "opnsense-firewall";
+                    static_configs = [{
+                      targets = [ "opnsense.${flake.lib.hostname}:9100" ];
+                      labels = {
+                        instance = "opnsense";
+                      };
+                    }];
+                  }
+                  {
+                    job_name = "traefik";
+                    static_configs = [{
+                      targets = [ "traefik.kube-system.svc.cluster.local:9100" ];
+                      labels = {
+                        instance = "traefik";
+                      };
+                    }];
+                  }
+                ];
+              };
+            };
+
+            alertmanager = {
+              # alertmanagerSpec.externalUrl = "https://alertmanager.${flake.lib.hostname}";
+              #  ingress = {
+              #    enabled = true;
+              #    hosts = [ "alertmanager.${flake.lib.hostname}" ];
+              #    pathType = "ImplementationSpecific";
+              #  };
+            };
+
+            grafana = {
+              defaultDashboardsEnabled = true;
+              defaultDashboardsTimezone = "America/New_York";
+              admin = {
+                existingSecret = "grafana-admin-credentials";
+                userKey = "admin-user";
+                passwordKey = "admin-password";
+              };
+              "grafana.ini" = {
+                "auth.anonymous" = {
+                  enabled = true;
+                  org_name = "Main Org.";
+                  org_role = "Viewer";
+                };
+                security.allow_embedding = true;
+                replicas = 1;
+              };
+              dashboardProviders."dashboardproviders.yaml" = {
+                apiVersion = 1;
+                providers = [{
+                  name = "default";
+                  orgId = 1;
+                  folder = "";
+                  type = "file";
+                  disableDeletion = true;
+                  editable = true;
+                  options.path = "/var/lib/grafana/dashboards/default";
+                }];
+              };
+              dashboards.default = {
+                node-exporter-full = {
+                  gnetId = 1860;
+                  revision = 31;
+                  datasource = "Prometheus";
+                };
+              };
+            };
+
+            prometheus-node-exporter.prometheus.monitor = {
+              enabled = true;
+              attachMetadata.node = true;
+              relabelings = [{
+                sourceLabels = [ "__meta_kubernetes_pod_node_name" ];
+                targetLabel = "instance";
+              }];
+            };
+
+          };
+        };
+      };
+      resources = {
+        ingressroute.grafana-dashboard = {
+          metadata.namespace = ns;
+          metadata.name = "grafana-dashboard";
+          spec = {
+            entryPoints = [ "websecure" ];
+            routes = [
+              {
+                match = "Host(`grafana.${flake.lib.hostname}`)";
+                kind = "Rule";
+                services = [
+                  {
+                    name = "kube-prometheus-stack-grafana";
+                    port = 80;
+                  }
+                ];
+              }
+            ];
+            tls = { };
+          };
+        };
+        ingressroute.prometheus-web = {
+          metadata = {
+            namespace = ns;
+            name = "prometheus-web";
+          };
+          spec = {
+            entryPoints = [ "websecure" ];
+            routes = [
+              {
+                match = "Host(`prometheus.${flake.lib.hostname}`)";
+                kind = "Rule";
+                services = [
+                  {
+                    name = "kube-prometheus-stack-prometheus";
+                    port = 9090;
+                  }
+                ];
+              }
+
+            ];
+          };
+        };
+        secrets.grafana-admin-credentials = {
+          metadata.namespace = ns;
+          metadata.name = "grafana-admin-credentials";
+          stringData = {
+            admin-user = "ref+sops://${flake.lib.secrets}/secrets/homelab.yaml#/grafana_admin";
+            admin-password = "ref+sops://${flake.lib.secrets}/secrets/homelab.yaml#/grafana_password";
+          };
+        };
+      };
+
+      customTypes = {
+        servicemonitors = {
+          attrName = "servicemonitors";
+          group = "monitoring.coreos.com";
+          version = "v1";
+          kind = "ServiceMonitor";
+        };
+        podmonitors = {
+          attrName = "podmonitors";
+          group = "monitoring.coreos.com";
+          version = "v1";
+          kind = "PodMonitor";
+        };
+        alertmanagers = {
+          attrName = "alertmanagers";
+          group = "monitoring.coreos.com";
+          version = "v1";
+          kind = "Alertmanager";
+        };
+        prometheus = {
+          attrName = "prometheus";
+          group = "monitoring.coreos.com";
+          version = "v1";
+          kind = "Prometheus";
+        };
+        prometheusrule = {
+          attrName = "prometheusrule";
+          group = "monitoring.coreos.com";
+          version = "v1";
+          kind = "PrometheusRule";
+        };
+        ingressroute = {
+          attrName = "ingressroute";
+          group = "traefik.io";
+          version = "v1alpha1";
+          kind = "IngressRoute";
+        };
+      };
+    };
+  };
+}
--- deployments/kubenix/monitoring/opnsense-exporter.nix
+++ deployments/kubenix/monitoring/opnsense-exporter.nix
@@ -19,58 +19,57 @@ in
 {
   imports = with kubenix.modules; [ k8s docker submodules ];
   submodules.imports = [ ../lib/namespaced.nix ];
-  submodules.instances.monitoring = {
-    submodule = "namespaced";
-    args.docker.images.opnsense-exporter = {
-      registry = "ghcr.io/athennamind";
-      name = "opnsense-exporter";
-      tag = "0.0.11";
-      # images.prometheus-monitoring.image = pkgs.callPackage ./../images/prometheus.nix { };
-    };
-    args.kubernetes.resources = {
-      deployments."${opnsenseExporter.label}" = {
-        metadata.labels.app = opnsenseExporter.label;
-        metadata.namespace = ns;
-        spec = {
-          replicas = 1;
-          selector.matchLabels.app = opnsenseExporter.label;
-          template = {
-            metadata.labels.app = opnsenseExporter.label;
-            spec = {
-              containers."${opnsenseExporter.label}" = {
-                inherit (opnsenseExporter) env secrets;
-                name = "${opnsenseExporter.label}";
-                image = config.docker.images.opnsense-exporter.path;
-                imagePullPolicy = opnsenseExporter.imagePolicy;
-                args = [
-                  "--opnsense.protocol=https"
-                  "--opnsense.address=opnsense.${flake.lib.hostname}"
-                  "--exporter.instance-label=instance1"
-                  "--web.listen-address=:8080"
-                ];
-                ports."http" = {
-                  containerPort = 8080;
-                  protocol = "TCP";
-                };
-                # resources.requests.cpu = opnsenseExporter.cpu;
-                # ports."${toString opnsenseExporter.port}" = { };
+  docker.images.opnsense-exporter = {
+    registry = "ghcr.io/athennamind";
+    name = "opnsense-exporter";
+    tag = "0.0.11";
+    # images.prometheus-monitoring.image = pkgs.callPackage ./../images/prometheus.nix { };
+  };
+
+  # Merge resources into the existing monitoring submodule instance
+  submodules.instances.monitoring.args.kubernetes.resources = {
+    deployments."${opnsenseExporter.label}" = {
+      metadata.labels.app = opnsenseExporter.label;
+      metadata.namespace = ns;
+      spec = {
+        replicas = 1;
+        selector.matchLabels.app = opnsenseExporter.label;
+        template = {
+          metadata.labels.app = opnsenseExporter.label;
+          spec = {
+            containers."${opnsenseExporter.label}" = {
+              inherit (opnsenseExporter) env secrets;
+              name = "${opnsenseExporter.label}";
+              image = config.docker.images.opnsense-exporter.path;
+              imagePullPolicy = opnsenseExporter.imagePolicy;
+              args = [
+                "--opnsense.protocol=https"
+                "--opnsense.address=opnsense.${flake.lib.hostname}"
+                "--exporter.instance-label=instance1"
+                "--web.listen-address=:8080"
+              ];
+              ports."http" = {
+                containerPort = 8080;
+                protocol = "TCP";
               };
+              # resources.requests.cpu = opnsenseExporter.cpu;
+              # ports."${toString opnsenseExporter.port}" = { };
             };
           };
         };
       };
-      services."${opnsenseExporter.label}" = {
-        metadata.namespace = ns;
-        spec = {
-          selector.app = "${opnsenseExporter.label}";
-          ports = [
-            {
-              name = "opnsense-exporter";
-              port = 8080;
-            }
-          ];
-          # ports."${toString opnsenseExporter.port}".targetPort = opnsenseExporter.port;
-        };
+    };
+    services."${opnsenseExporter.label}" = {
+      metadata.namespace = ns;
+      spec = {
+        selector.app = "${opnsenseExporter.label}";
+        ports = [
+          {
+            name = "opnsense-exporter";
+            port = 8080;
+          }
+        ];
+        # ports."${toString opnsenseExporter.port}".targetPort = opnsenseExporter.port;
       };
     };
   };
