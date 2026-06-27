81491825efce68eadce84184e8a27c5c49acb734
Andrew Plaza <github@andrewplaza.dev>
2026-06-27 14:36:31 -0400

    fix udev rules

--- machine-specific/tinyca/default.nix
+++ machine-specific/tinyca/default.nix
@@ -55,8 +55,7 @@
     wantedBy = [ "multi-user.target" ];
   };
   services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="usb", ENV{PRODUCT}=="1050/407/*", TAG+="systemd", SYMLINK+="yubikey"
-    ACTION=="remove", SUBSYSTEM=="usb", ENV{PRODUCT}=="1050/407/*", TAG+="systemd"
+    ENV{ID_VENDOR}=="Yubico", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0010|0111|0112|0113|0114|0115|0116|0401|0402|0403|0404|0405|0406|0407|0410", SYMLINK+="yubikey", TAG+="systemd"
   '';
 
   # yubikey needs polkit rules
